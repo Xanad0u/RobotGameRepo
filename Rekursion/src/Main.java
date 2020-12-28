10df0ae3df9601138ba5a1df3c7613fdb949dd01
Dennis Maaß <Dennis@maa-s.de>
2020-12-28 21:49:01 +0100

    test commit

--- Rekursion/src/Main.java
+++ Rekursion/src/Main.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 
-public class Main {		//main caller
+public class Main {		//main caller of the program
 
 	public static void main(String[] args) throws IOException{
 		MenuFrame menu = new MenuFrame();
