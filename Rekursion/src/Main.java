d3f51f025ed835bdf971e49f55609be73050c74c
Dennis Maaß <Dennis@maa-s.de>
2020-12-31 11:39:19 +0100

    initial commit of the buggy linkage branch (#2)

--- Rekursion/src/Main.java
+++ Rekursion/src/Main.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 
-public class Main {		//main caller of the game
+public class Main {		//main caller
 
 	public static void main(String[] args) throws IOException{
 		MenuFrame menu = new MenuFrame();
